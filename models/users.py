64062094a6156f8127a5a927d7841e3afeb82b56
nelala_ <strnadova.nela@linuxmail.org>
2022-08-11 10:31:25 +0200

    updated users model tests to work with new db toolkit

--- models/users.py
+++ models/users.py
@@ -5,7 +5,8 @@ from database.model import Model
 from database.string import String
 
 
-class Users(Model):
+class User(Model):
+    table_name: str = "users"
     id = Column(column_type=Integer(), primary_key=True)
     username = Column(column_type=String(30), required=True)
 
@@ -14,6 +15,9 @@ if __name__ == "__main__":
     from settings import ROOT_DIR
 
     test_db = Database(db_file=f"{ROOT_DIR}/test_database.db")
-    xXusrXx = Users.create_table(db=test_db)
+
+    nelala: User = User(username="nelala")
+    nelala.save(db=test_db)
+
     del test_db
 
