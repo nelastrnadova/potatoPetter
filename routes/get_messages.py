16994ef712489ab538cf237f5c6affe0c8d618ea
nelala_ <strnadova.nela@linuxmail.org>
2022-08-28 21:10:11 +0200

    fix get messages not returning string

--- routes/get_messages.py
+++ routes/get_messages.py
@@ -1,3 +1,6 @@
+import json
+
+from models.messages import Message
 from routes.BaseRoute import BaseRoute
 
 
@@ -6,7 +9,10 @@ class GetMessages(BaseRoute):
         return '{"supported_methods": "POST"}', 405
 
     def post(self):
-        if 'user_to_id' not in self.body:
+        if isinstance(self.body, str) or 'user_to_id' not in self.body:
             return '{"missing_arguments": "user_to_id"}', 400
-        # TODO: pagination
-        return '{"messages": "TODO"}', 200
+        # TODO: what happens if there arent any messages?
+        messages: [Message] = Message().get_instances_by_values(db=self.db, where_fields=["user_to_id"],
+                                                                where_values=[int(self.body['user_to_id'])])
+        messages_text: [str] = [message.text for message in messages]
+        return json.dumps({"messages": messages_text}), 200
