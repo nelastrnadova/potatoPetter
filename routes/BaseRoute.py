be9e3308509b355d6f3dfdc8f07d1ceba38f83a0
nelala_ <strnadova.nela@linuxmail.org>
2022-08-28 21:09:51 +0200

    converting string body to json to use in routes

--- routes/BaseRoute.py
+++ routes/BaseRoute.py
@@ -5,7 +5,7 @@ from database.database import Database
 
 class BaseRoute(ABC):
     def __init__(self, body: dict, db: Database):
-        self.body: dict = body
+        self.body: json = json.loads(body) if isinstance(body, str) else body
         self.db: Database = db
 
     @abstractmethod
