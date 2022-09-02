4d306d4462a7fb9bd7bab99b0917633a408be66c
nelala_ <strnadova.nela@linuxmail.org>
2022-09-02 23:08:00 +0200

    added password is already taken message

--- routes/register_user.py
+++ routes/register_user.py
@@ -14,8 +14,14 @@ class RegisterUser(BaseRoute):
             return BaseRoute.get_missing_params_message(missing_params)
 
         # TODO: test
-        new_user = User(username=self.body['username'], password=User.hash_pass(self.body['password']))
+        hashed_password: str = User.hash_pass(self.body['password'])
+        new_user = User(username=self.body['username'], password=hashed_password)
         if new_user.load(db=self.db, test_exists=True):
             return json.dumps({"errors": "username taken"}), 400, "application/json"
+
+        for user in User().get_all_instances(db=self.db):  # TODO: you may not like it, but this is what peak comedy looks like
+            if user.password == hashed_password:
+                return json.dumps({"errors": "password is already taken"}), 400, "application/json"
+
         new_user.save(db=self.db)
         return json.dumps({"new_user_id": new_user.get_cached_pk_value()}), 200, "application/json"
