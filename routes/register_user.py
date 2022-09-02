4bc4fcfec60a0ff9f61b7f823878f218b8a64f67
nelala_ <strnadova.nela@linuxmail.org>
2022-09-02 22:07:59 +0200

    cannot register new user if user already exists

--- routes/register_user.py
+++ routes/register_user.py
@@ -15,5 +15,7 @@ class RegisterUser(BaseRoute):
 
         # TODO: test
         new_user = User(username=self.body['username'], password=User.hash_pass(self.body['password']))
+        if new_user.load(db=self.db, test_exists=True):
+            return json.dumps({"errors": "username taken"}), 400
         new_user.save(db=self.db)
         return json.dumps({"new_user_id": new_user.get_cached_pk_value()}), 200
