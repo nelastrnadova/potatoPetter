a1c6ccedccb0e9dfdc4d11895d982b7993242d43
nelala_ <strnadova.nela@linuxmail.org>
2022-09-09 16:35:50 +0200

    lada auto

--- routes/lada.py
+++ routes/lada.py
@@ -5,7 +5,7 @@ from utils.file import File
 
 class Auto(BaseRoute):
     def get(self):
-        return File(f"{ROOT_DIR}/static/auto.html").get_content(), 200, "text/html"
+        return File(f"{ROOT_DIR}/static/lada.html").get_content(), 200, "text/html"
 
     def post(self):
         '{"supported_methods": "GET"}', 405, "application/json"
